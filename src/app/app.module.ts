2e4d649dcbd36ceb3988ec915aaf42697bd4eb58
Saroja Ammula <saroja.ammula@gmail.com>
2017-04-09 15:19:06 -0700

    add dashboard 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { AppRoutingModule }     from './app-routing.module';
+import { DashboardComponent }   from './dashboard.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+}
